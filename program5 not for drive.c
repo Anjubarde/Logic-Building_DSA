5d9b1ff21a08791c0f6af5afe6b748268adb591c
Anjali Barde <118333807+Anjubarde@users.noreply.github.com>
2024-10-23 11:47:34 +0530

    Update program5 not for drive.c

--- program5 not for drive.c
+++ program5 not for drive.c
@@ -2,13 +2,13 @@
 
 Step1 : Understand the problem statement 
 
-Step2 : Wrie the algorithm
+Step2 : Write the algorithm
 
 Step3 : Decide the programming language 
 
 Step4 : Write the program 
 
-Step5 : Test the rpogram 
+Step5 : Test the program 
 
 */
 
